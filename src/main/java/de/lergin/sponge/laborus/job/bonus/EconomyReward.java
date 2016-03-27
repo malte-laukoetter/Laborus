e5edf323248153e3efd78ba8b54552685a92d5a8
Lergin <Lergin@gmx.de>
2016-03-27 20:49:37 +0200

    changed cause thx. @Aaron1011

--- src/main/java/de/lergin/sponge/laborus/job/bonus/EconomyReward.java
+++ src/main/java/de/lergin/sponge/laborus/job/bonus/EconomyReward.java
@@ -1,5 +1,6 @@
 package de.lergin.sponge.laborus.job.bonus;
 
+import de.lergin.sponge.laborus.JobsMain;
 import de.lergin.sponge.laborus.job.JobBonus;
 import de.lergin.sponge.laborus.job.JobItem;
 import ninja.leaping.configurate.ConfigurationNode;
@@ -8,14 +9,15 @@ import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.event.cause.NamedCause;
 import org.spongepowered.api.service.economy.EconomyService;
-import org.spongepowered.api.text.Text;
 
 import java.math.BigDecimal;
 
 public class EconomyReward extends JobBonus{
-    EconomyService service = Sponge.getServiceManager().getRegistration(EconomyService.class).get().getProvider();
-    BigDecimal amountMax;
-    BigDecimal amountMin;
+    private final EconomyService service =
+            Sponge.getServiceManager().getRegistration(EconomyService.class).get().getProvider();
+    private final BigDecimal amountMax;
+    private final BigDecimal amountMin;
+    private final Cause cause = Cause.of(NamedCause.source(Sponge.getPluginManager().fromInstance(JobsMain.instance())));
 
     public EconomyReward(ConfigurationNode config) {
         super(config);
@@ -26,12 +28,10 @@ public class EconomyReward extends JobBonus{
 
     @Override
     public void useBonus(JobItem item, Player player) {
-        player.sendMessage(Text.of("sadsadsad"));
-
         service.getOrCreateAccount(player.getIdentifier()).get().deposit(
                 service.getDefaultCurrency(),
                 amountMax.divide(amountMin, BigDecimal.ROUND_FLOOR).multiply(BigDecimal.valueOf(Math.random())).add(amountMin),
-                Cause.of(NamedCause.source("Laborus"))
+                cause
         );
     }
 }
